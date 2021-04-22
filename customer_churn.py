2bbcb432c70229dcecfa039df388a037d32550d7
ARY BHISMA DIRGANTARA <67574629+bimaary31@users.noreply.github.com>
2021-04-22 14:56:55 +0700

    Add files via upload

---
+++ customer_churn.py
@@ -0,0 +1,233 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 23 20:42:56 2021
+
+@author: ARY - PC
+"""
+
+#Import library yang dibutuhkan 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import confusion_matrix,classification_report
+import pickle
+from pathlib import Path
+
+
+'''load data'''
+df_load=pd.read_csv(r'dqlab_telco_final.csv')
+
+#cek row &column
+
+print(df_load.shape)
+
+#lima teratas
+print(df_load.head())
+
+#info tipe data
+print(df_load.info)
+
+#jumlah data unik
+print(df_load.nunique())
+print(df_load.describe())
+
+'''exsplorasi data'''
+#Your codes here
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+ax.axis('equal')
+labels = ['Yes','No']
+churn = df_load.Churn.value_counts()
+ax.pie(churn, labels=labels, autopct='%.0f%%')
+plt.show()
+
+#creating bin in chart
+numerical_features = ['MonthlyCharges','TotalCharges','tenure']
+fig, ax = plt.subplots(1, 3, figsize=(15, 6))
+# Use the following code to plot two overlays of histogram per each numerical_features, use a color of blue and orange, respectively
+df_load[df_load.Churn == 'No'][numerical_features].hist(bins=20, color='blue', alpha=0.5, ax=ax)
+df_load[df_load.Churn == 'Yes'][numerical_features].hist(bins=20, color='orange', alpha=0.5, ax=ax)
+plt.show()
+
+# Your code goes here
+fig, ax = plt.subplots(3, 3, figsize=(14, 12))
+sns.countplot(data=df_load, x='gender', hue='Churn', ax=ax[0][0])
+sns.countplot(data=df_load, x='Partner', hue='Churn', ax=ax[0][1])
+sns.countplot(data=df_load, x='SeniorCitizen', hue='Churn', ax=ax[0][2])
+sns.countplot(data=df_load, x='PhoneService', hue='Churn', ax=ax[1][0])
+sns.countplot(data=df_load, x='StreamingTV', hue='Churn', ax=ax[1][1])
+sns.countplot(data=df_load, x='InternetService', hue='Churn', ax=ax[1][2])
+sns.countplot(data=df_load, x='PaperlessBilling', hue='Churn', ax=ax[2][0])
+plt.tight_layout()
+plt.show()
+
+'''cleaner area'''
+#Remove the unnecessary columns customerID & UpdatedAt
+cleaned_df=df_load.drop(['customerID','UpdatedAt'], axis=1)
+
+print(cleaned_df.head())
+
+
+'''handle columns non numeric to numeric'''
+def handle_non_numerical_data(cleaned_df):
+    columns = cleaned_df.columns.values
+    for column in columns:
+        text_digit_vals = {}
+        def convert_to_int(val):
+            return text_digit_vals[val]
+
+        if cleaned_df[column].dtype != np.int64:
+            column_contents = cleaned_df[column].values.tolist()
+            unique_elements = set(column_contents)
+            x = 0
+            for unique in unique_elements:
+                if unique not in text_digit_vals:
+                    text_digit_vals[unique] = x
+                    x+=1
+
+            cleaned_df[column] = list(map(convert_to_int, cleaned_df[column]))
+
+    return cleaned_df
+
+cleaned_df = handle_non_numerical_data(cleaned_df)
+print(cleaned_df.head())
+print(cleaned_df.describe())
+
+''' pembuatan model dengan splitting area'''
+# Predictor dan target
+X = cleaned_df.drop('Churn', axis = 1)
+y = cleaned_df['Churn']
+# Splitting train and test
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+# Print according to the expected result
+print('Jumlah baris dan kolom dari x_train adalah:', x_train.shape,', sedangkan Jumlah baris dan kolom dari y_train adalah:', y_train.shape)
+print('Prosentase Churn di data Training adalah:')
+print(y_train.value_counts(normalize=True))
+print('Jumlah baris dan kolom dari x_test adalah:', x_test.shape,', sedangkan Jumlah baris dan kolom dari y_test adalah:', y_test.shape)
+print('Prosentase Churn di data Testing adalah:')
+print(y_test.value_counts(normalize=True))
+
+#modelling
+log_model = LogisticRegression().fit(x_train, y_train)
+print('Model Logistic Regression yang terbentuk adalah: \n',log_model)
+
+'performance data train'
+y_train_pred = log_model.predict(x_train)
+# Print classification report
+print('Classification Report Training Model (Logistic Regression) :')
+print(classification_report(y_train, y_train_pred))
+
+'performance model'
+# Form confusion matrix as a DataFrame
+confusion_matrix_df = pd.DataFrame((confusion_matrix(y_train, y_train_pred)), ('No churn', 'Churn'), ('No churn', 'Churn'))
+
+# Plot confusion matrix
+plt.figure()
+heatmap = sns.heatmap(confusion_matrix_df, annot=True, annot_kws={'size': 14}, fmt='d', cmap='YlGnBu')
+heatmap.yaxis.set_ticklabels(heatmap.yaxis.get_ticklabels(), rotation=0, ha='right', fontsize=14)
+heatmap.xaxis.set_ticklabels(heatmap.xaxis.get_ticklabels(), rotation=0, ha='right', fontsize=14)
+
+plt.title('Confusion Matrix for Training Model\n(Logistic Regression)', fontsize=18, color='darkblue')
+plt.ylabel('True label', fontsize=14)
+plt.xlabel('Predicted label', fontsize=14)
+plt.show()
+
+'''performance data testing'''
+# Predict
+y_test_pred = log_model.predict(x_test)
+# Print classification report 
+print('Classification Report Testing Model (Logistic Regression):')
+print(classification_report(y_test, y_test_pred))
+
+# Form confusion matrix as a DataFrame
+confusion_matrix_df = pd.DataFrame((confusion_matrix(y_test, y_test_pred)), ('No churn', 'Churn'), ('No churn', 'Churn'))
+
+# Plot confusion matrix
+plt.figure()
+heatmap = sns.heatmap(confusion_matrix_df, annot=True, annot_kws={'size': 14}, fmt='d', cmap='YlGnBu')
+heatmap.yaxis.set_ticklabels(heatmap.yaxis.get_ticklabels(), rotation=0, ha='right', fontsize=14)
+heatmap.xaxis.set_ticklabels(heatmap.xaxis.get_ticklabels(), rotation=0, ha='right', fontsize=14)
+
+plt.title('Confusion Matrix for Testing Model\n(Logistic Regression)\n', fontsize=18, color='darkblue')
+plt.ylabel('True label', fontsize=14)
+plt.xlabel('Predicted label', fontsize=14)
+plt.show()
+
+'''RANDOM FOREST CLASSIFIER'''
+#Train the model
+rdf_model = RandomForestClassifier().fit(x_train, y_train)
+print('RDF',rdf_model)
+
+#PREDICT
+y_train_pred = rdf_model.predict(x_train)
+print('Classification Report Training Model (Random Forest) :')
+print(classification_report(y_train, y_train_pred))
+
+#PERFORMANCE
+# Form confusion matrix as a DataFrame
+confusion_matrix_df = pd.DataFrame((confusion_matrix(y_train, y_train_pred)), ('No churn', 'Churn'), ('No churn', 'Churn'))
+
+# Plot confusion matrix
+plt.figure()
+heatmap = sns.heatmap(confusion_matrix_df, annot=True, annot_kws={'size': 14}, fmt='d', cmap='YlGnBu')
+heatmap.yaxis.set_ticklabels(heatmap.yaxis.get_ticklabels(), rotation=0, ha='right', fontsize=14)
+heatmap.xaxis.set_ticklabels(heatmap.xaxis.get_ticklabels(), rotation=0, ha='right', fontsize=14)
+
+plt.title('Confusion Matrix for Training Model\n(Random Forest)', fontsize=18, color='darkblue')
+plt.ylabel('True label', fontsize=14)
+plt.xlabel('Predicted label', fontsize=14)
+plt.show()
+
+
+'''GRADIENT BOOSTING CLASSIFIER'''
+gbt_model = GradientBoostingClassifier().fit(x_train, y_train)
+print(gbt_model)
+# Predict
+y_train_pred = gbt_model.predict(x_train)
+# Print classification report
+print('Classification Report Training Model (Gradient Boosting):')
+print(classification_report(y_train, y_train_pred))
+
+# Form confusion matrix as a DataFrame
+confusion_matrix_df = pd.DataFrame((confusion_matrix(y_train, y_train_pred)), ('No churn', 'Churn'), ('No churn', 'Churn'))
+
+# Plot confusion matrix
+plt.figure()
+heatmap = sns.heatmap(confusion_matrix_df, annot=True, annot_kws={'size': 14}, fmt='d', cmap='YlGnBu')
+heatmap.yaxis.set_ticklabels(heatmap.yaxis.get_ticklabels(), rotation=0, ha='right', fontsize=14)
+heatmap.xaxis.set_ticklabels(heatmap.xaxis.get_ticklabels(), rotation=0, ha='right', fontsize=14)
+
+plt.title('Confusion Matrix for Training Model\n(Gradient Boosting)', fontsize=18, color='darkblue')
+plt.ylabel('True label', fontsize=14)
+plt.xlabel('Predicted label', fontsize=14)
+plt.show()
+
+
+# Predict
+y_test_pred = gbt_model.predict(x_test)
+# Print classification report
+print('Classification Report Testing Model (Gradient Boosting):')
+print(classification_report(y_test, y_test_pred))
+
+# Form confusion matrix as a DataFrame
+confusion_matrix_df = pd.DataFrame((confusion_matrix(y_test, y_test_pred)), ('No churn', 'Churn'), ('No churn', 'Churn'))
+
+# Plot confusion matrix
+plt.figure()
+heatmap = sns.heatmap(confusion_matrix_df, annot=True, annot_kws={'size': 14}, fmt='d', cmap='YlGnBu')
+heatmap.yaxis.set_ticklabels(heatmap.yaxis.get_ticklabels(), rotation=0, ha='right', fontsize=14)
+heatmap.xaxis.set_ticklabels(heatmap.xaxis.get_ticklabels(), rotation=0, ha='right', fontsize=14)
+
+plt.title('Confusion Matrix for Testing Model\n(Gradient Boosting)', fontsize=18, color='darkblue')
+plt.ylabel('True label', fontsize=14)
+plt.xlabel('Predicted label', fontsize=14)
+plt.show()
+
+'''PEMILIHAN MODEL TERBAIK'''
+print(log_model)
